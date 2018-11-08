9aea89ac38868cbe527fe122a2705db6e574114f
Steve Smith <steve@kentsmiths.com>
2018-11-08 02:30:18 -0500

    finishing refactoring

--- solutions/problem7.js
+++ solutions/problem7.js
@@ -1,30 +1,8 @@
 'use strict';
 
-// generators in js
-// https://medium.com/dailyjs/a-simple-guide-to-understanding-javascript-es6-generators-d1c350551950
-function* getIntegers(start = 2) {
-    while (true) yield start++;
-}
+var getPrimes = require('../lib/getPrimes');
 
-function* getPrimes(start = 2) {
-    var numberSequence = getIntegers(start);
-    while (true) {
-        var nextNum = numberSequence.next().value;
-        if (isPrime(nextNum)) {
-            yield nextNum;
-        }
-    }
-}
-
-var isPrime = function(input) {
-    if (input == 2) return true;
-    for (var i = 2; i <= Math.sqrt(input); i++) {
-        if (input % i == 0) return false;
-    }
-    return true;
-}
-
-var getPrime = function(index) {
+var getPrime = function (index) {
     var primes = getPrimes(2);
     var result;
     for (var i = 0; i < index; i++) {
@@ -33,8 +11,7 @@ var getPrime = function(index) {
     return result.value;
 }
 
-exports.solve = function() {
-    // TODO: Implement solution
+exports.solve = function () {
     console.log('The 10,001st prime number is...');
     return getPrime(10001);
 }
