e5cf0f9dc0bdf97c7467f6373aa26a0782853080
Steve Smith <steve@kentsmiths.com>
2019-01-28 17:20:49 -0500

    Working on prob 16; strint lib is too slow.

--- solutions/problem16.js
+++ solutions/problem16.js
@@ -1,13 +1,37 @@
 'use strict';
-
+let strint = require('../lib/strint');
 /*
 
 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
 What is the sum of the digits of the number 2^1000?
 
-*/
+http://2ality.com/2012/07/large-integers.html
+All numbers are floating point; max of 53 bits.
 
-exports.solve = function () {
-    return 0;
+*/
+function Solution() {
+    function powerof2(num) {
+        var initialValue = "1";
+        for (var i = 1; i <= num; i++) {
+            initialValue = strint.mul(initialValue, "2");
+        }
+        return initialValue;
+    }
+
+    Solution.prototype.solveForN = function(exponent) {
+        var bigNumberString = powerof2(exponent);
+        var sumOfDigits = 0;
+        for (var i = 0; i < bigNumberString; i++) {
+            sumOfDigits = sumOfDigits + parseInt(bigNumberString[i]);
+        }
+        return sumOfDigits;
+    };
+
+    // called by index.js for this problem
+    Solution.prototype.solve = function() {
+        return this.solveForN(56);
+    };
 }
+
+module.exports = Solution;
