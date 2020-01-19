1863133c4b77b7f7cdc910d94dffcdce36c9cb85
Steve Smith <steve@kentsmiths.com>
2020-01-19 18:01:38 -0500

    solved problem 22

--- solutions/problem22.js
+++ solutions/problem22.js
@@ -18,6 +18,19 @@ What is the total of all the name scores in the file?
 
 function Solution() {
 
+    Solution.prototype.alphaValue = function(input) {
+        let alphaValues = {
+            A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8,I:9,J:10,
+            K:11,L:12,M:13,N:14,O:15,P:16,Q:17,R:18,
+            S:19,T:20,U:21,V:22,W:23,X:24,Y:25,Z:26
+        }
+        let result = 0;
+        for (var i = 0; i < input.length; i++) {
+            result = result + alphaValues[input[i]];
+        }
+    
+        return result;
+    }
     // called by index.js for this problem
     Solution.prototype.solve = function() {
 
@@ -30,8 +43,12 @@ function Solution() {
             .split(',')
             .sort();
         
-        console.log(names);
-        return 0;
+        let namevalueSum = 0;
+        names.forEach((val,index) => {
+            let nameValue = this.alphaValue(val) * (index+1);
+            namevalueSum += nameValue;
+        });
+        return namevalueSum;
     };
 }
 
