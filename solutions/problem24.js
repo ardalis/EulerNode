598c56d04ba73e2858f65cc23a7893446b4ee699
Steve Smith <steve@kentsmiths.com>
2020-04-11 09:26:04 -0400

    Adding templates for 24,25

---
+++ solutions/problem24.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/*
+
+A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of
+ the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we 
+ call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+
+012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+*/
+
+function Solution() {
+
+    Solution.prototype.getPermutations = function getAbundantNumbers(start = 2) {
+            // todo: generate permutations in alphabetical order
+        }
+    }
+
+    // called by index.js for this problem
+    Solution.prototype.solve = function() {
+
+        
+        return 0;
+    };
+}
+
+module.exports = Solution;
---
+++ solutions/problem25.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/*
+
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+*/
+
+function Solution() {
+
+    Solution.prototype.getPermutations = function getAbundantNumbers(start = 2) {
+            // todo: generate permutations in alphabetical order
+        }
+    }
+
+    // called by index.js for this problem
+    Solution.prototype.solve = function() {
+
+        
+        return 0;
+    };
+}
+
+module.exports = Solution;
